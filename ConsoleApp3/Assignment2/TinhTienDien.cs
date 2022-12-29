70a1474a7b6c1e68b6707f63d1e05252157972e7
Administrator <tranvanthanh060699@gmail.com>
2022-12-29 18:08:34 +0700

    up

--- ConsoleApp3/Assignment2/TinhTienDien.cs
+++ ConsoleApp3/Assignment2/TinhTienDien.cs
@@ -7,6 +7,24 @@ public class TinhTienDien
 	}
 	public int DinhMuc { get; set; };
 	public int Soluong { get; set; };
-	public int DonGia { get;set };
+	public int DonGia { get; set; };
+	public int KWVuotDinhMuc { get; set; }
+	public int DonGiaMoi { get; set; }
+	public int ThanhTien { get; set; }
+	public int KhachHangVN()
+	{
+		if(Soluong < DinhMuc)
+		{
+			return ThanhTien = Soluong * DonGia;
+		}
+		else
+		{
+			return ThanhTien = DonGia * DinhMuc + (Soluong - DinhMuc) * DonGiaMoi;
+		}
+	}
+	public int KhachHangNcNgoai()
+	{
+		return ThanhTien = Soluong * DonGia; 
+	}
 
 }
