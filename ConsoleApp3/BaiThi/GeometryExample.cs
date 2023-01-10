0d7b31749b15048f71f14bbc358e5970cf844acc
Administrator <tranvanthanh060699@gmail.com>
2023-01-10 18:19:41 +0700

    up

---
+++ ConsoleApp3/BaiThi/GeometryExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TT2203E.BaiThi
+{
+    internal class GeometryExample
+    {
+    }
+}
